97167c9ce4740c914b1329b1e58d3355f927a8f4
Pedro Vieira <pedrovieiradev@hotmail.com>
2015-09-02 18:08:11 +0100

    First fingerprint matching iOS 9 draft

--- Hoko/HOKDeferredDeeplinking.m
+++ Hoko/HOKDeferredDeeplinking.m
@@ -16,12 +16,22 @@
 #import "HOKNotificationObserver.h"
 #import "HOKObserver.h"
 
+#if __IPHONE_OS_VERSION_MIN_REQUIRED >= 90000
+#import <SafariServices/SafariServices.h>
+#endif
+
+
 NSString *const HOKDeferredDeeplinkingNotFirstRun = @"isNotFirstRun";
 NSString *const HOKDeferredDeeplinkingPath = @"installs/ios";
+NSString *const HOKFingerprintMatchingPath = @"match?idfa=%@";
 
 @interface HOKDeferredDeeplinking ()
+#if __IPHONE_OS_VERSION_MIN_REQUIRED >= 90000
+<SFSafariViewControllerDelegate>
+#endif
 
 @property (nonatomic, strong) NSString *token;
+@property (nonatomic, copy) void (^handler)(NSString *deeplink);
 
 @end
 
@@ -38,24 +48,51 @@ NSString *const HOKDeferredDeeplinkingPath = @"installs/ios";
 - (void)requestDeferredDeeplink:(void (^)(NSString *))handler {
   BOOL isFirstRun = ![[HOKUtils objectForKey:HOKDeferredDeeplinkingNotFirstRun] boolValue];
   if (isFirstRun) {
-    [HOKUtils saveObject:@YES key:HOKDeferredDeeplinkingNotFirstRun];
-    [HOKNetworking postToPath:[HOKNetworkOperation urlFromPath:HOKDeferredDeeplinkingPath] parameters:self.json token:self.token successBlock:^(id json) {
-      NSString *deeplink = [json objectForKey:@"deeplink"];
-      if (deeplink && [deeplink isKindOfClass:[NSString class]] && handler) {
-        handler(deeplink);
-      }
-    } failedBlock:^(NSError *error) {
-      HOKErrorLog(error);
-    }];
+    self.handler = handler;
+    
+#if __IPHONE_OS_VERSION_MIN_REQUIRED >= 90000
+    NSString *fingerprintString = [NSString stringWithFormat:HOKFingerprintMatchingPath, [HOKDevice device].uid];
+    SFSafariViewController *safariViewController = [[SFSafariViewController alloc] initWithURL:[NSURL URLWithString:[HOKNetworkOperation urlFromPath:fingerprintString]]];
+    safariViewController.delegate = self;
+#else
+    [self hok_requestDeferredDeeplink];
+#endif
   }
+}
+
+- (void)hok_requestDeferredDeeplink {
+  [HOKUtils saveObject:@YES key:HOKDeferredDeeplinkingNotFirstRun];
+  [HOKNetworking postToPath:[HOKNetworkOperation urlFromPath:HOKDeferredDeeplinkingPath] parameters:self.json token:self.token successBlock:^(id json) {
+    NSString *deeplink = [json objectForKey:@"deeplink"];
+    if (deeplink && [deeplink isKindOfClass:[NSString class]] && self.handler) {
+      self.handler(deeplink);
+    }
+  } failedBlock:^(NSError *error) {
+    HOKErrorLog(error);
+  }];
 }
 
 - (NSDictionary *)json {
   return @{@"device": @{@"os_version": [HOKDevice device].systemVersion,
                         @"device_type": [HOKDevice device].platform,
                         @"language": [HOKDevice device].systemLanguage.lowercaseString,
-                        @"screen_size": [HOKDevice device].screenSize}
+                        @"screen_size": [HOKDevice device].screenSize,
+                        
+                        /* ADDED NOW */
+                        @"uid": [HOKDevice device].uid
+                        }
            };
 }
+
+
+
+#pragma mark - SFSafariViewController delegate method
+#if __IPHONE_OS_VERSION_MIN_REQUIRED >= 90000
+- (void)safariViewController:(SFSafariViewController *)controller didCompleteInitialLoad:(BOOL)didLoadSuccessfully {
+  [self hok_requestDeferredDeeplink];
+}
+#endif
+
+
 
 @end
