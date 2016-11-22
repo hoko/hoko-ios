0abd0ca3b1b8aadaa5a70d01bece25014d4864ba
Hugo Sequeira <hugoandresequeira@gmail.com>
2016-11-22 15:57:06 +0000

    Rollback fingerprinting features

--- Hoko/HOKDeferredDeeplinking.m
+++ Hoko/HOKDeferredDeeplinking.m
@@ -7,6 +7,7 @@
 //
 
 #import "HOKDeferredDeeplinking.h"
+
 #import "HOKUtils.h"
 #import "HOKLogger.h"
 #import "HOKDevice.h"
@@ -14,17 +15,13 @@
 #import "HOKNetworkOperation.h"
 #import "HOKNotificationObserver.h"
 #import "HOKObserver.h"
-#import "HOKNavigation.h"
-#import "HOKIframe.h"
 
 NSString *const HOKDeferredDeeplinkingNotFirstRun = @"isNotFirstRun";
 NSString *const HOKDeferredDeeplinkingPath = @"installs/ios";
-NSString *const HOKFingerprintMatchingPath = @"fingerprints/match";
 
 @interface HOKDeferredDeeplinking ()
 
 @property (nonatomic, strong) NSString *token;
-@property (nonatomic, copy) void (^handler)(NSString *deeplink);
 
 @end
 
@@ -41,35 +38,24 @@ NSString *const HOKFingerprintMatchingPath = @"fingerprints/match";
 - (void)requestDeferredDeeplink:(void (^)(NSString *))handler {
   BOOL isFirstRun = ![[HOKUtils objectForKey:HOKDeferredDeeplinkingNotFirstRun] boolValue];
   if (isFirstRun) {
-    self.handler = handler;
-    NSString *fingerprintURL = [NSString stringWithFormat:@"%@?uid=%@", [HOKNetworkOperation urlFromPath:HOKFingerprintMatchingPath], [HOKDevice device].uid];
-    
-    [HOKIframe requestPageWithURL:fingerprintURL completion:^{
-      [self requestDeferredDeeplink];
+    [HOKUtils saveObject:@YES key:HOKDeferredDeeplinkingNotFirstRun];
+    [HOKNetworking postToPath:[HOKNetworkOperation urlFromPath:HOKDeferredDeeplinkingPath] parameters:self.json token:self.token successBlock:^(id json) {
+      NSString *deeplink = [json objectForKey:@"deeplink"];
+      if (deeplink && [deeplink isKindOfClass:[NSString class]] && handler) {
+        handler(deeplink);
+      }
+    } failedBlock:^(NSError *error) {
+      HOKErrorLog(error);
     }];
   }
 }
 
-- (void)requestDeferredDeeplink {
-  [HOKUtils saveObject:@YES key:HOKDeferredDeeplinkingNotFirstRun];
-  [HOKNetworking postToPath:[HOKNetworkOperation urlFromPath:HOKDeferredDeeplinkingPath] parameters:self.json token:self.token successBlock:^(id json) {
-    NSString *deeplink = [json objectForKey:@"deeplink"];
-    if (deeplink && [deeplink isKindOfClass:[NSString class]] && self.handler) {
-      self.handler(deeplink);
-    }
-  } failedBlock:^(NSError *error) {
-    HOKErrorLog(error);
-  }];
-}
-
 - (NSDictionary *)json {
-  return @{@"device": @{@"os_version": [HOKUtils jsonValue:[HOKDevice device].systemVersion],
-                        @"device_type": [HOKUtils jsonValue:[HOKDevice device].platform],
-                        @"language": [HOKUtils jsonValue:[HOKDevice device].systemLanguage.lowercaseString],
-                        @"screen_size": [HOKUtils jsonValue:[HOKDevice device].screenSize],
-                        @"uid": [HOKUtils jsonValue:[HOKDevice device].uid] }
+  return @{@"device": @{@"os_version": [HOKDevice device].systemVersion,
+                        @"device_type": [HOKDevice device].platform,
+                        @"language": [HOKDevice device].systemLanguage.lowercaseString,
+                        @"screen_size": [HOKDevice device].screenSize}
            };
 }
 
 @end
-
